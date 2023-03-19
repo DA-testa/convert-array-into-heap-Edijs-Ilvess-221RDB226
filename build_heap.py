8da654dfa0df9d3696f18388ac2a1c651479f446
Edijs-Ilvess-221RDB226 <125735046+Edijs-Ilvess-221RDB226@users.noreply.github.com>
2023-03-19 16:53:35 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,7 +2,7 @@
 
 def build_heap(data):
     swaps = []
-    n==len(data)
+    n=len(data)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
     for i in range(n//2,-1,-1):
