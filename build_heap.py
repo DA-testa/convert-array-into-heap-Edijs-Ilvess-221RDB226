a098016d193bd7965bca58d9f2332842618c53ec
Edijs-Ilvess-221RDB226 <125735046+Edijs-Ilvess-221RDB226@users.noreply.github.com>
2023-03-19 16:28:22 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,10 +1,24 @@
 # python3
 
-
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
+    for i in range(n//2,-1,-1):
+        current=i
+        first_con=2*current+1
+        second_con=2*current+2
+        while n>first_con:
+            tiny=current
+            if data[first_con]<data[tiny]:
+                tiny=first_con
+            if n>second_con:
+                if data[second_con]<data[tiny]:
+                    tiny=second_con
+            if tiny!=current:
+                swap=data[tiny]
+                data[tiny]=data[current]
+                data[current]=swap
 
 
     return swaps
@@ -13,13 +27,23 @@ def build_heap(data):
 def main():
     
     # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    # add another input for I or F
+    input_IF=input()
+    if input_IF[0]=="I":
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif input_IF[0]=="F":
+        tests=input()
+        with open("test/"+tests) as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    else:
+        print("I/F error")
+        
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
